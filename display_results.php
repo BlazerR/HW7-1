d582ed2269b35a956fa5b2ee8ad841ce16173f6d
BlazerR <br229@njit.edu>
2017-03-01 19:13:37 -0500

    4

--- display_results.php
+++ display_results.php
@@ -3,8 +3,8 @@
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
     // get the rest of the data for the form
-     $password = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_EMAIL);
-      $phone = filter_input(INPUT_POST, 'phone', FILTER_VALIDATE_EMAIL);
+     $password = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_PASSWORD);
+      $phone = filter_input(INPUT_POST, 'phone', FILTER_VALIDATE_PHONE);
     // for the heard_from radio buttons,
     // display a value of 'Unknown' if the user doesn't select a radio button
 
